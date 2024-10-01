4b72af37dc5caa0ed80299a258c013b618ff2b0b
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2024-10-01 19:34:58 +0000

    meow

---
+++ src/functions/getIcon.ts
@@ -0,0 +1,56 @@
+import { ArgType, NativeFunction } from "@tryforge/forgescript";
+import { expect } from '@jest/globals';
+const mcs = require('node-mcstatus');
+
+interface Result {
+    icon: string | null;
+}
+
+export default new NativeFunction({
+    name: '$getIcon',
+    description: 'Get Java Icon URL',
+    version: '1.0.0',
+    brackets: true,
+    unwrap: true,
+    args: [
+        {
+            name: 'host',
+            description: 'Host Domain',
+            type: ArgType.String,
+            required: false,
+            rest: false
+        },
+        {
+            name: 'port',
+            description: 'The Host Port',
+            type: ArgType.Number,
+            required: true,
+            rest: false
+        },
+        {
+            name: 'options',
+            description: 'display Java Options',
+            type: ArgType.Boolean,
+            required: true,
+            rest: false
+        }
+    ],
+    async execute (ctx, [host, port, options]) {
+        try {
+            const result: Result = await mcs.statusJava(host, port, options);
+            
+            // Test the result object
+            expect(typeof result.icon === 'string' || result.icon === null).toBe(true);
+            
+            if (typeof result.icon === 'string') {
+                expect(result.icon.length).toBeGreaterThan(0);
+                expect(result.icon.startsWith('data:image/png;base64,')).toBe(true);
+            }
+            
+            return this.success();
+        } catch (error) {
+            console.error("Error fetching status:", error);
+            return this.customError("Failed to fetch status");
+        }
+    }
+});
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { ForgeExtension } from '@tryforge/forgescript';
+
+
+export class ForgeMC extends ForgeExtension {
+    name = 'forge.mc';
+    description = 'A forgescript extension that allows you to create and edit images with ease.';
+    version = '1.0.0';
+
+    public init () {
+        this.load(__dirname + '/functions');
+    };
+};
